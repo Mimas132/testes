ece6a85a48d71bff99468c97f8db4fe43049f81e
root <root@jessie.raw>
2017-09-21 13:51:02 +0000

    agora deu certo

--- calculadora.sh
+++ calculadora.sh
@@ -9,14 +9,15 @@ clear
 echo "digite os segundos digitos"
 	read S1
 clear
-echo -n " o resultado é : "
-read -p "$(($P1 $OPERACAO $S1))" RESUL
+RESUL=$(($P1 $OPERACAO $S1))
+echo  " o resultado é : " $RESUL
 echo "Deseja continuar a operação?"
 read O2
 if [ $O2 == "sim" ] ;
 then
-	echo " Qual o sina da nova operação? "
+	echo " Qual o sinal da nova operação? "
 read OPERACAO2
 	echo " quais os novos digitos? "
 read P2
-		echo a resposta é : $((
+		echo a resposta é : $(($RESUL $OPERACAO2 $P2))
+fi
